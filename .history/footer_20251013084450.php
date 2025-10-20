2c8177cfb6d5f689f7ec9b7b1fc3ba66239f8562
pilarfolgar <pilarfsanturio@gmail.com>
2025-10-20 08:28:00 -0300

    ddd

--- .history/footer_20251013084450.php
+++ .history/footer_20251013084450.php
@@ -1,6 +1,5 @@
 <?php
 ?>
-<link rel="stylesheet" href="style.css">
 <footer class="footer">
     &copy; 2025 Instituto Tecnológico Superior de Paysandú | Contacto: evolutionit2008@gmail.com <br>
     Desarrollado por Martina Campopiano, Pilar Folgar y Julieta Dotti
